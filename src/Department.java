60b2ef8fac82002acaed2a61cc5256bbf44fe369
Alexey Studenikin <avstdn@gmail.com>
2019-06-06 11:46:54 +0300

    Replaced ArrayList with TreeSet for better performance

--- src/Department.java
+++ src/Department.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.*;
 
 class Department {
 
@@ -8,38 +7,35 @@ class Department {
     * @return массив строк отсортированных и дополненных кодов подразделений
     */
     public static String[] getSortedDepartments(String[] departments) {
-        ArrayList<String> result = new ArrayList<>();
+        Set<String> departmentSet = new TreeSet<>();
 
-        // Проходим по исходному массиву и разбиваем каждый элемент на массив по
-        // символу "\"
+        // Проходим по исходному массиву и разбиваем каждый элемент еще
+        // на один массив по "\"
         for (String department : departments) {
             String[] parts = department.split("\\\\");
-            StringBuilder part = new StringBuilder();
+            StringBuilder temp = new StringBuilder();
 
-            // Добавляем недостающие коды подразделений в результирующий массив,
-            // если их там нет
-            for (int i = 0; i < parts.length; i++) {
-                if (part.length() > 0) part.append("\\").append(parts[i]);
-                else part.append(parts[i]);
-
-                if (!result.contains(part.toString())) result.add(part.toString());
+            // Добавляем недостающие коды подразделений в TreeSet
+            for (String part : parts) {
+                if (temp.length() > 0) temp.append("\\").append(part);
+                else temp.append(part);
+                departmentSet.add(temp.toString());
             }
         }
 
-        // Переопределяем исходный компаратор и сортируем элементы в
-        // необходимом порядке
-        result.sort(new Comparator<String>() {
-            @Override
-            public int compare(String str1, String str2) {
-                for (int i = 0; i < str1.length() && i < str2.length(); i++) {
+        // Переопределяем исходный компаратор
+        Set<String> sortedSet =  new TreeSet<>((str1, str2) -> {
+            for (int i = 0; i < str1.length() && i < str2.length(); i++) {
                     if ((int)str1.charAt(i) != (int)str2.charAt(i)) {
                         return (int)str2.charAt(i) - (int)str1.charAt(i);
                     }
-                }
-                return 0;
             }
+            return str1.length() - str2.length();
         });
 
-        return result.toArray(new String[0]);
+        // Сортируем элементы в заданном порядке
+        sortedSet.addAll(departmentSet);
+
+        return sortedSet.toArray(new String[0]);
     }
 }
